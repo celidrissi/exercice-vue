ce1575b627c22bee95a5ace200f13e180f4c6e1a
Chafik EL IDRISSI <celidrissi@inpi.fr>
2022-05-09 15:05:27 +0200

    composition api

--- src/composition/save.js
+++
@@ -1,8 +0,0 @@
-export function save() {
-    if (this.action === "edit") {
-      this.$store.dispatch("tasks/updateDishe", this.dishe);
-    } else {
-      this.$store.dispatch("tasks/addDishe", this.dishe);
-    }
-    this.$emit('close');
-  };
